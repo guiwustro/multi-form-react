1b82727f7cdd03ed3285d157f691d27294fa17d0
Guilherme <gui.wustro@gmail.com>
2023-05-29 07:45:02 -0300

    feat: create summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,51 @@
+import { useSectionContext } from "../../contexts/SectionContext";
+import {
+	Paper,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
+	Box,
+} from "@mui/material";
+import { percentualMask } from "../../utils/mask";
+const Summary = () => {
+	const { sections } = useSectionContext();
+
+	return (
+		<Box sx={{ display: "flex", justifyContent: "center" }}>
+			<Paper sx={{ maxWidth: "600px" }}>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell align="center">Intervals</TableCell>
+							<TableCell align="center">Amount</TableCell>
+							<TableCell align="center">Value Type</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{sections.map((section, index) => {
+							const amount =
+								section.valueType === "percentage"
+									? percentualMask(section.amount!)
+									: section.amount;
+							return (
+								<TableRow hover key={index}>
+									<TableCell>
+										{section.interval.map((interval) => (
+											<p>{`${interval.startDate} to ${interval.endDate}`}</p>
+										))}
+									</TableCell>
+									<TableCell>{amount}</TableCell>
+									<TableCell>{section.valueType}</TableCell>
+								</TableRow>
+							);
+						})}
+					</TableBody>
+				</Table>
+			</Paper>
+		</Box>
+	);
+};
+
+export default Summary;
